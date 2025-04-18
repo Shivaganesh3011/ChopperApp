686cc65e532795273e6cf30b7a97731b9479402e
shiva <shivaganeshmittapalli301120@gmail.com>
2025-04-18 16:07:29 +0530

    Initial commit with login screen

--- ChopperApp/ViewController.swift
+++ ChopperApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupPasswordToggle()
+    }
+    func setupPasswordToggle() {
+        let toggleButton = UIButton(type: .custom)
+        toggleButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+        toggleButton.tintColor = .gray
+        toggleButton.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        toggleButton.addTarget(self, action: #selector(togglePasswordVisibility), for: .touchUpInside)
+        passwordTextField.rightView = toggleButton
+        passwordTextField.rightViewMode = .always
+    }
+    @objc func togglePasswordVisibility(_ sender: UIButton) {
+        passwordTextField.isSecureTextEntry.toggle()
+        
+        let imageName = passwordTextField.isSecureTextEntry ? "eye.slash.fill" : "eye.fill"
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
     }
 
 
 }
-
